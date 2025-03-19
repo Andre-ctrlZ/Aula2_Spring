8e0bf7d67443166c6e0d0ffd44896381ce51bd8d
Aluno Dendezeiros <aluno.den@senaiba.com.br>
2025-03-19 19:13:42 -0300

    Primeiro commit para a aula de String.

---
+++ AulaDoisSpring/src/test/java/com/example/AulaDoisSpring/AulaDoisSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AulaDoisSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AulaDoisSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
